20b90d328b14e7fd560bfa5a3b8c70bf53b63e4b
Jose <enriquesais123@gmail.com>
2023-11-13 23:42:07 -0500

    reasignar redeclarar

---
+++ src/scope/reasignacion_redeclaracion.js
@@ -0,0 +1,28 @@
+var firstName; // undefined
+firstName = 'oscar';
+console.log(firstName);
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Pedro'; // reasignar
+console.log(lastName);
+
+var secondName = 'david'; // declarando / asignando
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// let
+
+let fruit = 'Apple'; // declarando / asignando
+fruit = 'kiwi'; // reasignar
+console.log(fruit);
+
+// const
+
+const animal = 'dog'; // declara / asigna
+// animal = 'cat'; // reasignar
+console.log(animal);
+
+const vehicles=[];
+vehicles.push('car');
+
+console.log(vehicles);
